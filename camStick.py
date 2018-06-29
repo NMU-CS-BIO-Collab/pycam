93ecf7cb418439271281784520640f7937086337
jonathonlefler <lefler.t.jonathon@gmail.com>
2018-06-29 17:04:54 +0000

    Added basic functionality

--- camStick.py
+++ camStick.py
@@ -1,3 +1,21 @@
 # @program - camStick
 # This will be for the rig
 # to look into high nest
+
+from picamera import PiCamera
+from time import sleep
+
+def main():
+    camera = PiCamera()
+
+    camera.start_preview()
+    sleep(10)
+    camera.stop_preview()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Shuting down")
+        #GPIO.cleanup() #Will need this when we implement LEDs
+sys.exit(0)
